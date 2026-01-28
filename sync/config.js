873f9a6aa85a42bc27f93df343a5ca02611e2d03
Chrisok9788 <finanzas200599@gmail.com>
2026-01-28 10:53:09 -0300

    Create config.js

---
+++ sync/config.js
@@ -0,0 +1,16 @@
+// sync/config.js
+// Archivo de configuración general
+// NO contiene lógica, solo datos de conexión
+
+export const CONFIG = {
+  SCANNTECH: {
+    BASE_URL: "https://api.scanntech.com.uy", 
+    API_KEY: "PENDIENTE_DE_DEFINIR"
+  },
+
+  SHEETS: {
+    SPREADSHEET_ID: "ID_DEL_GOOGLE_SHEET",
+    SHEET_PRODUCTOS: "PRODUCTOS",
+    SHEET_PENDIENTES: "PENDIENTES"
+  }
+};
